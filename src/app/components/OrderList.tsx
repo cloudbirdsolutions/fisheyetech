7abf04897122ce700f824377b90312c10ad05641
rpk555 <sivaneswari.gp@gmail.com>
2024-05-05 12:07:08 +0530

    User Screen Updates

--- src/app/components/OrderList.tsx
+++ src/app/components/OrderList.tsx
@@ -2,27 +2,16 @@
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
-import { ColorPaletteProp } from '@mui/joy/styles';
-import Box from '@mui/joy/Box';
 import Avatar from '@mui/joy/Avatar';
 import Chip from '@mui/joy/Chip';
-import Link from '@mui/joy/Link';
-import Divider from '@mui/joy/Divider';
 import IconButton from '@mui/joy/IconButton';
-import Typography from '@mui/joy/Typography';
-import List from '@mui/joy/List';
 import ListItem from '@mui/joy/ListItem';
 import ListItemContent from '@mui/joy/ListItemContent';
 import ListItemDecorator from '@mui/joy/ListItemDecorator';
 import ListDivider from '@mui/joy/ListDivider';
-import Menu from '@mui/joy/Menu';
-import MenuButton from '@mui/joy/MenuButton';
-import MenuItem from '@mui/joy/MenuItem';
-import Dropdown from '@mui/joy/Dropdown';
 
 
 
-import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
 import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 import BlockIcon from '@mui/icons-material/Block';
 import AutorenewRoundedIcon from '@mui/icons-material/AutorenewRounded';
@@ -36,6 +25,13 @@ import { AppDispatch } from '../Store/store';
 import { API_BASE_URL } from '../config';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Box from '@mui/joy/Box';
+import List from '@mui/joy/List';
+
+import {stableSort, getComparator} from '@/app/helper/sorting';
+import Typography from '@mui/joy/Typography';
+
+type Order = "asc" | "desc";
 
 
 
@@ -47,6 +43,8 @@ export default function OrderList(props:any) {
 
   const dispatch = useDispatch<AppDispatch>();
   const router = useRouter();
+  const [order, setOrder] = React.useState<Order>("desc");
+
 
   const HandleDeleteFunction = (id:any) => {
     try {
@@ -98,7 +96,7 @@ export default function OrderList(props:any) {
 
   return (
     <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
-      {listItems && listItems.map((listItem: any) => (
+      {listItems && stableSort(listItems, getComparator(order, "id")).map((listItem: any) => (
         <List
           key={listItem.id}
           size="sm"
--- src/app/components/OrderTable.tsx
+++ src/app/components/OrderTable.tsx
@@ -19,55 +19,17 @@ import { deleteuser } from '../Reducers/DeleteUserSlice';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TableSection from './Common/TableSection';
+import {stableSort, getComparator} from '@/app/helper/sorting';
 
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = 'asc' | 'desc';
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key,
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string },
-) => number {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
-function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
+type Order = "asc" | "desc";
 
 export default function OrderTable(props:any) {
   const createuser = useSelector((state:RootState) => state?.createusers?.data);
   const deleteusers = useSelector((state:RootState) => state?.deleteusers?.data);
   const edituser = useSelector((state:RootState) => state?.editusers?.data);
 
-  const [order, setOrder] = React.useState<Order>('desc');
-  const [open, setOpen] = React.useState(false);
+  const [order, setOrder] = React.useState<Order>("desc");
+
   const [rows, setRows] = React.useState([
     { id: "", name: "", userName: "", status: "", password: "", action :"" },
   ]);
@@ -108,7 +70,8 @@ export default function OrderTable(props:any) {
         });
 
         if (!response.ok) {
-          throw new Error('Failed to fetch user details: ' + response.statusText);
+          const errorData = await response.json();
+          toast.error(errorData.message)
         }
 
         const data = await response.json();
