ec1a51d9bd81369a467bd747054143dbd49fbc22
rpk555 <sivaneswari.gp@gmail.com>
2024-05-20 21:46:13 +0530

    Authentication Verify

--- src/app/hooks/useAuth/index.js
+++ src/app/hooks/useAuth/index.js
@@ -1,7 +1,7 @@
 "use client" ;
 import React from "react";
 //import Cookies from "universal-cookie";
-//import { verifyJwtToken } from "@/libs/auth";
+import { verifyJwtToken } from "@/app/libs/auth";
 
 export function useAuth() {
   const [auth, setAuth] = React.useState(localStorage.getItem('accessToken'));
@@ -9,8 +9,12 @@ export function useAuth() {
   const getVerifiedtoken = async () => {
    // const cookies = new Cookies();
    const token = localStorage.getItem("accessToken") ?? null;
-    //const verifiedToken = await verifyJwtToken(token);
-    setAuth(token);
+    const verifiedToken = await verifyJwtToken(token);
+    !verifiedToken ?
+      setAuth(token) 
+      : 
+      setAuth(null)
+
   };
   React.useEffect(() => {
     getVerifiedtoken();
