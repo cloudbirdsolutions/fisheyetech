c51e1e91f7acef738b8345b7d33b7349118be779
baruitsolutions <barunitsolutions@hotmail.com>
2024-05-21 12:03:11 +0530

    commemnt download users option

--- src/app/(landing)/users/page.tsx
+++ src/app/(landing)/users/page.tsx
@@ -54,13 +54,13 @@ export default function Users() {
               >
                 Add New User
               </Button>
-              <Button
+              {/* <Button
                 color="primary"
                 startDecorator={<DownloadRoundedIcon />}
                 size="sm"
               >
                 Download Excel
-              </Button>
+              </Button> */}
             </Stack>
           </Box>
 
